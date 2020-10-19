c922d832bdbfa056975afae226195ee91bfcfca3
unknown <vivisol.plopez@gmail.com>
2020-10-19 10:57:42 -0300

    Locallibreria:Creando Formularios

--- locallibreria/locallibreria/settings.py
+++ locallibreria/locallibreria/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalogo',
-    
+    'catalogo.apps.CatalogoConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'locallibreria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
